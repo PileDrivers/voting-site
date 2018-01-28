e1f7ccd3f5bcd8efe5b1abac3aa7b548f10d7b7a
MB <bardwell@ualberta.ca>
2018-01-27 19:02:23 -0700

    gPIo setup

--- bin/sockettoRPiinstructions.py
+++ bin/sockettoRPiinstructions.py
@@ -1,3 +1,13 @@
-var a = 'fwd'
-
+import RPi.GPIO as GPIO
+import time
 import socketio
+
+GPIO.setmode(GPIO.BOARD)
+
+FETChannels = [2, 3];
+GPIO.setup(FETChannels, GPIO.OUT)
+GPIO.setup(FETChannels, GPIO.OUT, initial=GPIO.LOW)
+
+if fwd:
+    GPIO.output(channel, state)
+    time.sleep(5) 
